cd4b151fab2fa32a1356a3537667aa45b46477c8
VladimirE <wortex2014@bk.ru>
2022-10-03 17:28:56 +0400

    vse

--- zadanie29/Program.cs
+++ zadanie29/Program.cs
@@ -4,7 +4,7 @@
 6, 1, 33 -> [6, 1, 33]
 ввод элеменетов через консоль (через запятую. использовать метод string.Split()) */
 
-Console.Write("Введите элементы массива:\t");
+Console.Write("Введите элементы массива: ");
 int[] arr = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
 var result = string.Join(", ", arr);
 Console.Write($"[{result}]");
